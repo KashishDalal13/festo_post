a7a4d8a6f6b29b8282e1fbc61904ab5269d29206
kashishdalal1 <kashish.metaloop@gmail.com>
2024-02-28 12:58:26 +0530

    onboard

---
+++ lib/OnBoarding/on_boarding_info.dart
@@ -0,0 +1,7 @@
+class OnBoardingInfo{
+  late final String title;
+  late final String descriptions;
+  late final String image;
+
+  OnBoardingInfo({required this.title, required this.descriptions, required this.image});
+}
