5580504b0842ad026ef161ce0a0ec9ff50171c9b
kashishdalal1 <kashish.metaloop@gmail.com>
2024-02-28 12:58:26 +0530

    RL 28/02/2024 13:04

---
+++ lib/OnBoarding/on_boarding_view.dart
@@ -0,0 +1,71 @@
+import 'package:festo_post/OnBoarding/on_BoardingItems.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = OnBoardingItems();
+    final pageController = PageController();
+    return Scaffold(
+      bottomSheet: Container(
+        color: Colors.white,
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+
+            // skip button
+            TextButton(
+              onPressed: () => pageController.jumpToPage(controller.items.length-1),
+              child: const Text("Skip"),
+            ),
+
+            // Indicator
+            SmoothPageIndicator(
+              controller: pageController,
+              count: controller.items.length,
+              effect: const WormEffect(
+                activeDotColor: Colors.purple
+              ),
+            ),
+
+            // next button
+            TextButton(
+              onPressed: () => pageController.nextPage(duration: const Duration(seconds: 1), curve: Curves.easeIn),
+              child: const Text("Next"),
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 15),
+        child: PageView.builder(
+            itemCount: controller.items.length,
+            controller: pageController,
+            itemBuilder: (context, index) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(controller.items[index].image),
+                  const SizedBox(height: 15),
+                  Text(
+                    controller.items[index].title,
+                    style: const TextStyle(
+                        fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 15),
+                  Text(
+                    controller.items[index].descriptions,
+                    style: const TextStyle(color: Colors.grey, fontSize: 17),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              );
+            }),
+      ),
+    );
+  }
+}
