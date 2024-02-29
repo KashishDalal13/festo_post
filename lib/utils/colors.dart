a8f4a4ef52b12ea42782a74bf097a28602ab5d44
payal1904 <payalpatel144@gmail.com>
2024-02-29 14:41:51 +0530

    RL 28/02/2024 17:08 onboarding design change

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color purple = Colors.purple, yellowFFA500 = const Color(0XffFFA500), greyB6B6B6 = const Color(0xFFB6B6B6), grey = const Color(0xff5c5c5c);
+  static Color purple = Colors.purple,
+      yellowFFA500 = const Color(0XffFFA500),
+      greyB6B6B6 = const Color(0xFFB6B6B6),
+      grey = const Color(0xff5c5c5c);
 }
