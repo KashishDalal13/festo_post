5e76422a7d90cf0b0f4ed9cfab41150bc5b6aa93
kashishdalal1 <kashish.metaloop@gmail.com>
2024-02-29 11:08:32 +0530

    skip color change

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color purple = Colors.purple, yellowFFA500 = const Color(0XffFFA500), greyB6B6B6 = const Color(0xFFB6B6B6);
+  static Color purple = Colors.purple, yellowFFA500 = const Color(0XffFFA500), greyB6B6B6 = const Color(0xFFB6B6B6), grey = const Color(0xff5c5c5c);
 }
