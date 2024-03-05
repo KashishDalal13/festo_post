b75574c859f7b5e5468b6e86670e32b4eb3b3ca2
payal1904 <payalpatel144@gmail.com>
2024-03-05 17:37:28 +0530

    payal

--- lib/module/login_module/view/add_details_view.dart
+++ lib/module/login_module/view/add_details_view.dart
@@ -1,5 +1,6 @@
 import 'package:festo_post/module/login_module/provider/register_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../utils/colors.dart';
 import '../../../utils/string.dart';
@@ -13,14 +14,74 @@ class AddDetails extends StatelessWidget {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
       appBar: AppBar(
-          leading: IconButton(onPressed: () => provider.onBack(), icon: const Icon(Icons.arrow_back_ios_rounded, size: 20)),
+          leading: IconButton(
+              onPressed: () => provider.onBack(),
+              icon: const Icon(Icons.arrow_back_ios_rounded, size: 20)),
           centerTitle: true,
-          title: Text(StrRef.addDetails, style: TextStyle(fontFamily: 'Lato', fontSize: 18, color: ColorRef.black202020))),
+          title: Text(StrRef.addDetails,
+              style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 18,
+                  color: ColorRef.black202020))),
       resizeToAvoidBottomInset: false,
-      body: Column(
-        children: [Text("Add details")],
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () => print("upload"),
+              child: Container(
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: ColorRef.greyEDEDED,
+                ),
+                child: Icon(Icons.add,color: ColorRef.blue0250A4,),
+              ),
+            ),
+            const SizedBox(height: 5,),
+            Text(StrRef.uploadPNG,style: const TextStyle(fontFamily: 'Lato',fontWeight: FontWeight.w400,fontSize: 13)),
+            const SizedBox(height: 10),
+            Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(15), color: ColorRef.greyEDEDED),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                      hintText: provider.addDetail.first['label'],
+                      hintStyle: TextStyle(fontFamily: 'Lato', fontSize: 15, color: ColorRef.grey929292),
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: SvgPicture.asset(SvgPath.gift),
+                      ),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 5),
+            GestureDetector(
+              onTap: () =>provider.onSkipOrSubmit(),
+              child: Container(
+                height: 40,
+                margin: const EdgeInsets.symmetric(horizontal: 40),
+                decoration: BoxDecoration(color: ColorRef.yellowFFA500, borderRadius: BorderRadius.circular(15)),
+                alignment: Alignment.center,
+                child: Text(
+                  StrRef.submit,
+                  style: TextStyle(color: ColorRef.black202020, fontSize: 18, fontWeight: FontWeight.bold, fontFamily: 'Lato'),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
