600ec22891298ad4366b2c98ec13d3c90af91a1c
payal1904 <payalpatel144@gmail.com>
2024-03-06 18:01:10 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/module/dashboard_module/view/dashboard_view.dart

--- lib/module/dashboard_module/view/dashboard_view.dart
+++ lib/module/dashboard_module/view/dashboard_view.dart
@@ -32,14 +32,23 @@ class dashboardView extends StatelessWidget {
             appBar: AppBar(
               leading: IconButton(
                 onPressed: () {},
-                icon: Icon(Icons.menu, size: 24, color: BoolRef.themeChange ? ColorRef.white : ColorRef.black202020),
+                icon: Icon(Icons.menu,
+                    size: 24,
+                    color: BoolRef.themeChange
+                        ? ColorRef.white
+                        : ColorRef.black202020),
               ),
               centerTitle: true,
-              title: Text(StrRef.registerTitle2, style: const TextStyle(fontSize: 20, fontFamily: 'Lato', fontWeight: FontWeight.w400)),
+              title: Text(StrRef.registerTitle2,
+                  style: const TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'Lato',
+                      fontWeight: FontWeight.w400)),
               actions: [
                 IconButton(
                   onPressed: () {},
-                  icon: SvgPicture.asset(SvgPath.profile, width: 24, height: 24),
+                  icon:
+                      SvgPicture.asset(SvgPath.profile, width: 24, height: 24),
                 ),
               ],
             ),
@@ -47,10 +56,19 @@ class dashboardView extends StatelessWidget {
             body: ListView(
               children: [
                 CarouselSlider(
-                  items: imgList.map((url) => Padding(padding: const EdgeInsets.symmetric(horizontal: 15), child: Image.asset(SvgPath.carousel))).toList(),
+                  items: imgList
+                      .map((url) => Image.asset(SvgPath.carousel))
+                      .toList(),
                   options: CarouselOptions(
-                      onPageChanged: (index, reason) => provider.onPageChanged(index), initialPage: 0, enableInfiniteScroll: true, viewportFraction: 1, height: 160, padEnds: true),
+                      onPageChanged: (index, reason) =>
+                          provider.onPageChanged(index),
+                      initialPage: 0,
+                      enableInfiniteScroll: true,
+                      viewportFraction: 1,
+                      height: 160,
+                      padEnds: true),
                 ),
+                const SizedBox(height: 15,),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: imgList.map((url) {
@@ -61,7 +79,9 @@ class dashboardView extends StatelessWidget {
                       margin: const EdgeInsets.symmetric(horizontal: 2.0),
                       decoration: BoxDecoration(
                         shape: BoxShape.circle,
-                        color: index == provider.current ? ColorRef.yellowFFA500 : ColorRef.greyEDEDED,
+                        color: index == provider.current
+                            ? ColorRef.yellowFFA500
+                            : ColorRef.greyEDEDED,
                       ),
                     );
                   }).toList(),
@@ -75,8 +95,17 @@ class dashboardView extends StatelessWidget {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text(StrRef.upcomingEvents, style: const TextStyle(fontWeight: FontWeight.w600, fontFamily: 'Lato', fontSize: 16)),
-                          Text(StrRef.viewAll, style: TextStyle(fontWeight: FontWeight.w400, fontFamily: 'Lato', fontSize: 13, color: ColorRef.blue0250A4)),
+                          Text(StrRef.upcomingEvents,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontFamily: 'Lato',
+                                  fontSize: 16)),
+                          Text(StrRef.viewAll,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontFamily: 'Lato',
+                                  fontSize: 13,
+                                  color: ColorRef.blue0250A4)),
                         ],
                       ),
                       SizedBox(
@@ -98,7 +127,9 @@ class dashboardView extends StatelessWidget {
                                     width: 60,
                                     decoration: BoxDecoration(
                                       borderRadius: BorderRadius.circular(15),
-                                      color: index == provider.eventIndex ? ColorRef.yellowFFA500 : ColorRef.yellowFFEDCC,
+                                      color: index == provider.eventIndex
+                                          ? ColorRef.yellowFFA500
+                                          : ColorRef.yellowFFEDCC,
                                     ),
                                     child: Center(
                                       child: Text(
@@ -121,9 +152,13 @@ class dashboardView extends StatelessWidget {
                         child: ListView.separated(
                             scrollDirection: Axis.horizontal,
                             shrinkWrap: true,
-                            itemCount: provider.addDate[provider.eventIndex]['imageList'].length,
+                            itemCount: provider
+                                .addDate[provider.eventIndex]['imageList']
+                                .length,
                             itemBuilder: (context, index) {
-                              return Image.asset(provider.addDate[provider.eventIndex]['imageList'][index]);
+                              return Image.asset(
+                                  provider.addDate[provider.eventIndex]
+                                      ['imageList'][index]);
                             },
                             separatorBuilder: (context, index) {
                               return const SizedBox(width: 5);
@@ -137,7 +172,8 @@ class dashboardView extends StatelessWidget {
                             Expanded(
                               child: Divider(
                                 thickness: 1, // Adjust thickness as needed
-                                color: ColorRef.grey717171, // Adjust color as needed
+                                color: ColorRef
+                                    .grey717171, // Adjust color as needed
                               ),
                             ),
                             Text(
@@ -148,11 +184,13 @@ class dashboardView extends StatelessWidget {
                                 fontSize: 16,
                               ),
                             ),
-                            const SizedBox(width: 10), // Adjust the width as needed
+                            const SizedBox(width: 10),
+                            // Adjust the width as needed
                             Expanded(
                               child: Divider(
                                 thickness: 1, // Adjust thickness as needed
-                                color: ColorRef.grey717171, // Adjust color as needed
+                                color: ColorRef
+                                    .grey717171, // Adjust color as needed
                               ),
                             ),
                           ],
@@ -167,26 +205,42 @@ class dashboardView extends StatelessWidget {
                           return Column(
                             children: [
                               Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
                                 children: [
                                   Text(
                                     provider.addTodayEvent[index]['label'],
-                                    style: TextStyle(fontWeight: FontWeight.w600, fontFamily: 'Lato', color: ColorRef.brownBE7B00, fontSize: 15),
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        fontFamily: 'Lato',
+                                        color: ColorRef.brownBE7B00,
+                                        fontSize: 15),
                                   ),
                                   Text(
                                     StrRef.viewAll,
-                                    style: TextStyle(fontWeight: FontWeight.w400, fontFamily: 'Lato', fontSize: 13, color: ColorRef.blue0250A4),
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w400,
+                                        fontFamily: 'Lato',
+                                        fontSize: 13,
+                                        color: ColorRef.blue0250A4),
                                   ),
                                 ],
                               ),
                               Container(
                                 height: 120,
-                                margin: const EdgeInsets.only(top: 8,bottom: 16),
+                                margin:
+                                    const EdgeInsets.only(top: 8, bottom: 16),
                                 child: ListView.separated(
                                     scrollDirection: Axis.horizontal,
-                                    itemCount: provider.addTodayEvent[index]['imageList'].length,
-                                    itemBuilder: (context, innerIndex) => Image.asset(provider.addTodayEvent[index]['imageList'][innerIndex]),
-                                    separatorBuilder: (context, index) => const SizedBox(width: 8)),
+                                    itemCount: provider
+                                        .addTodayEvent[index]['imageList']
+                                        .length,
+                                    itemBuilder: (context, innerIndex) =>
+                                        Image.asset(
+                                            provider.addTodayEvent[index]
+                                                ['imageList'][innerIndex]),
+                                    separatorBuilder: (context, index) =>
+                                        const SizedBox(width: 8)),
                               ),
                             ],
                           );
@@ -215,7 +269,7 @@ class dashboardView extends StatelessWidget {
                       ),
                       SizedBox(
                         height: 50,
-                        child: ListView.builder(
+                        child: ListView.separated(
                           scrollDirection: Axis.horizontal,
                           shrinkWrap: true,
                           itemCount: provider.addTrendingEvent.length,
@@ -239,7 +293,8 @@ class dashboardView extends StatelessWidget {
                                     ),
                                     child: Center(
                                       child: Text(
-                                        provider.addTrendingEvent[index]['label'],
+                                        provider.addTrendingEvent[index]
+                                            ['label'],
                                       ),
                                     ),
                                   ),
@@ -248,23 +303,28 @@ class dashboardView extends StatelessWidget {
                               ],
                             );
                           },
+                          separatorBuilder: (BuildContext context, int index) {
+                            return const SizedBox(width: 5);
+                          },
                         ),
                       ),
-                      Row(
-                        children: [
-                          ...provider.addTrendingEvent[provider.trendingIndex]
-                          ['imageList']
-                              .map(
-                                (e) => Container(
-                                margin: const EdgeInsets.symmetric(
-                                    horizontal: 5, vertical: 8),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(15),
-                                ),
-                                child: Image.asset(e)),
-                          )
-                              .toList(),
-                        ],
+                      SizedBox(
+                        height: 100,
+                        child: ListView.separated(
+                            scrollDirection: Axis.horizontal,
+                            shrinkWrap: true,
+                            itemCount: provider
+                                .addTrendingEvent[provider.trendingIndex]
+                                    ['imageList']
+                                .length,
+                            itemBuilder: (context, index) {
+                              return Image.asset(provider
+                                      .addTrendingEvent[provider.trendingIndex]
+                                  ['imageList'][index]);
+                            },
+                            separatorBuilder: (context, index) {
+                              return const SizedBox(width: 15);
+                            }),
                       ),
                       const SizedBox(height: 15),
                       Row(
@@ -274,7 +334,8 @@ class dashboardView extends StatelessWidget {
                             child: Divider(
                               endIndent: 15.0,
                               thickness: 1, // Adjust thickness as needed
-                              color: ColorRef.grey717171, // Adjust color as needed
+                              color:
+                                  ColorRef.grey717171, // Adjust color as needed
                             ),
                           ),
                           Center(
@@ -287,12 +348,14 @@ class dashboardView extends StatelessWidget {
                               ),
                             ),
                           ),
-                          const SizedBox(width: 10), // Adjust the width as needed
+                          const SizedBox(width: 10),
+                          // Adjust the width as needed
                           Expanded(
                             child: Divider(
                               indent: 8.0,
                               thickness: 1, // Adjust thickness as needed
-                              color: ColorRef.grey717171, // Adjust color as needed
+                              color:
+                                  ColorRef.grey717171, // Adjust color as needed
                             ),
                           ),
                         ],
@@ -302,10 +365,10 @@ class dashboardView extends StatelessWidget {
                       ),
                       SizedBox(
                         height: 100,
-                        child: ListView.builder(
+                        child: ListView.separated(
                           scrollDirection: Axis.horizontal,
                           shrinkWrap: true,
-                          itemCount: provider.addTrendingEvent.length,
+                          itemCount: provider.addSeasonOffers.length,
                           itemBuilder: (BuildContext context, int index) {
                             return Column(
                               children: [
@@ -313,15 +376,22 @@ class dashboardView extends StatelessWidget {
                                   margin: const EdgeInsets.symmetric(
                                       horizontal: 8, vertical: 1),
                                   child: Image.asset(
-                                    provider.addSeasonOffers[index]["imageList"],
+                                    provider.addSeasonOffers[index]
+                                        ["imageList"],
                                     height: 70,
                                     width: 70,
                                   ),
                                 ),
-                                Text(provider.addSeasonOffers[index]["label"]),
+                                Text(
+                                  provider.addSeasonOffers[index]["label"],
+                                  style: const TextStyle(
+                                      fontFamily: "Lato",
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w400),
+                                ),
                               ],
                             );
-                          },
+                          }, separatorBuilder: (BuildContext context, int index) {  return const SizedBox(width: 5); },
                         ),
                       ),
                     ],
