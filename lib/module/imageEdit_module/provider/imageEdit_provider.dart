4e0c1498e8c866e6796a784a6963271dc445b1fc
Roshni-pc <roshnitailor47@gmail.com>
2024-03-16 10:36:10 +0530

    RL 16/03/2024 10:35

--- lib/module/imageEdit_module/provider/imageEdit_provider.dart
+++ lib/module/imageEdit_module/provider/imageEdit_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:math';
 import 'dart:io';
+import 'package:festo_post/module/imageEdit_module/stackboard_custom_item.dart';
 import 'package:festo_post/utils/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -76,6 +77,7 @@ class ImageEditProvider extends ChangeNotifier {
   Map<String, dynamic> activeItem = {};
   bool inAction = false;
   double? currentScale, currentRotation;
+
   void edit({required int index, required BuildContext context}) async {
     currentIndex = index.toString();
     debugPrint("${EditDetails[index]} $currentIndex");
@@ -93,71 +95,48 @@ class ImageEditProvider extends ChangeNotifier {
           return Dialog(
             backgroundColor: Colors.transparent,
             // shadowColor: Colors.black.withOpacity(0.3),
-             elevation: 0,
+            elevation: 0,
             alignment: Alignment.center,
             // color: ColorRef.transparent,
-            child: SizedBox(height: 260,width: 300,
+            child: SizedBox(
+              height: 260,
+              width: 300,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   GestureDetector(
-                    onTap: (){Navigator.of(context).pop();},
+                    onTap: () => NavigationService.goBack(),
                     child: Container(
-                      decoration: BoxDecoration(
-                        color: ColorRef.white,
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child:SvgPicture.asset(SvgPath.cancel),
+                      decoration: BoxDecoration(color: ColorRef.white, borderRadius: BorderRadius.circular(15)),
+                      child: SvgPicture.asset(SvgPath.cancel),
                     ),
                   ),
-                  const SizedBox(height: 10,),
+                  const SizedBox(height: 10),
                   Container(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 50, horizontal: 110),
+                    padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 110),
                     decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(5),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 2,
-                          blurRadius: 5,
-                          offset: const Offset(0, 3),
-                        ),
-                      ],
+                      boxShadow: [BoxShadow(color: Colors.grey.withOpacity(0.5), spreadRadius: 2, blurRadius: 5, offset: const Offset(0, 3))],
                     ),
                     child: Column(
                       children: [
                         TextField(
-                          decoration: const InputDecoration(
-                            hintText: 'Write Here',
-                            border: InputBorder.none,
-                          ),
-                          onChanged: (value) {
-                            text = value; // Update text on change
-                          },
+                          decoration: const InputDecoration(hintText: 'Write Here', border: InputBorder.none),
+                          onChanged: (value) => text = value,
                         ),
                       ],
                     ),
                   ),
                   const SizedBox(height: 20),
-                  // Add space between text field and button
                   GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).pop(text); // Return text when done
-                    },
+                    onTap: () => NavigationService.goBack(),
                     child: Container(
-                      margin: EdgeInsets.symmetric(horizontal: 90),
+                      margin: const EdgeInsets.symmetric(horizontal: 90),
                       alignment: Alignment.center,
                       padding: const EdgeInsets.symmetric(vertical: 10),
-                      decoration: BoxDecoration(
-                        color: ColorRef.yellowFFA500,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Text(
-                        'Add',
-                        style: TextStyle(color: ColorRef.black202020),
-                      ),
+                      decoration: BoxDecoration(color: ColorRef.yellowFFA500, borderRadius: BorderRadius.circular(10)),
+                      child: Text('Add', style: TextStyle(color: ColorRef.black202020)),
                     ),
                   ),
                 ],
@@ -169,43 +148,36 @@ class ImageEditProvider extends ChangeNotifier {
 
       if (result != null) {
         Completer<bool> completer = Completer<bool>();
-
-        boardController.add(
-          AdaptiveText(
+        boardController.add(CustomItem(
+                childWidget: GestureDetector(
+                    onTap: () {
+                      showModalBottomSheet<void>(
+                        context: context,
+                        builder: (BuildContext context) => const SizedBox(
+                          height: 300, // Adjust height as needed
+                          child: Center(child: Text('This is a bottom sheet', style: TextStyle(fontSize: 20.0))),
+                        ),
+                      );
+                    },
+                    child: Text(result)))
+            /*AdaptiveText(
             result,
             tapToEdit: false,
-              style: const TextStyle(fontWeight: FontWeight.w600,fontSize: 25,fontFamily: 'Lato'),
+            style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 25, fontFamily: 'Lato'),
             onDel: () {
               // Open bottom sheet here
               showModalBottomSheet<void>(
                 context: context,
-                builder: (BuildContext context) {
-                  return Container(
+                builder: (BuildContext context) => const SizedBox(
                     height: 300, // Adjust height as needed
-                    child: const Center(
-                      child: Text(
-                        'This is a bottom sheet',
-                        style: TextStyle(fontSize: 20.0),
-                      ),
-                    ),
-                  );
-                },
-              ).then((value) {
-                completer.complete(true);
-              });
-
-              // Return the Future<bool>
+                    child: Center(child: Text('This is a bottom sheet', style: TextStyle(fontSize: 20.0))),
+                  ),
+              ).then((value) => completer.complete(true));
               return completer.future;
             },
-          ),
-        );
+          ),*/
+            );
       }
-
-
-
-
-
-
     } else if (index == 2) {
       ImagePicker().pickImage(source: ImageSource.gallery).then(
         (value) {
@@ -294,8 +266,7 @@ class ImageEditProvider extends ChangeNotifier {
     activeItem['top'] += (details.focalPointDelta.dy);
     activeItem['left'] = (activeItem['left'] as double).clamp(2, width - w);
     activeItem['top'] = (activeItem['top'] as double).clamp(2, height - h);
-    activeItem['position'] =
-        Offset(activeItem['left'].toDouble(), activeItem['top'].toDouble());
+    activeItem['position'] = Offset(activeItem['left'].toDouble(), activeItem['top'].toDouble());
     activeItem['rotation'] = details.rotation + currentRotation!;
     debugPrint("$activeItem");
     double scale = max(min(details.scale * currentScale!, 2), 0.3);
