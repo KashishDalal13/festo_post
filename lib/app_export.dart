494c0bc638f41ee44f51281844176c8b14b2fc23
payal1904 <payalpatel144@gmail.com>
2024-03-28 12:30:52 +0530

    Payal 2/03/2024 12.00

--- lib/app_export.dart
+++ lib/app_export.dart
@@ -1,5 +1,49 @@
 export 'package:flutter/material.dart';
 export 'package:flutter/services.dart';
+export 'package:festo_post/main.dart';
 
 // Widget
 export 'package:festo_post/widget/toast.dart';
+
+//utils
+export 'package:festo_post/utils/bool.dart';
+export 'package:festo_post/utils/colors.dart';
+export 'package:festo_post/utils/routes.dart';
+export 'package:festo_post/utils/string.dart';
+export 'package:festo_post/utils/text_style.dart';
+export 'package:festo_post/utils/theme.dart';
+
+//theme_chnage
+export 'package:festo_post/theme_change/theme_settings.dart';
+
+//shared
+export 'package:festo_post/shared/injector.dart';
+export 'package:festo_post/shared/prefs_key.dart';
+
+//provider
+export 'package:festo_post/module/splash_module/provider/splash_screen_provider.dart';
+export 'package:festo_post/module/profile_module/provider/profile_provider.dart';
+export 'package:festo_post/module/on_boarding/onboarding_provider.dart';
+export 'package:festo_post/module/login_module/provider/register_provider.dart';
+export 'package:festo_post/module/info_module/provider/info_provider.dart';
+export 'package:festo_post/module/imageEdit_module/provider/imageEdit_provider.dart';
+export 'package:festo_post/module/frameEditing_module/provider/frameEditing_provider.dart';
+export 'package:festo_post/module/dashboard_module/provider/dashboard_provider.dart';
+
+//view
+export 'package:festo_post/module/splash_module/view/splash_screen_view.dart';
+export 'package:festo_post/module/profile_module/view/profile_view.dart';
+export 'package:festo_post/module/on_boarding/on_boarding_info.dart';
+export 'package:festo_post/module/on_boarding/on_boarding_view.dart';
+export 'package:festo_post/module/login_module/view/add_details_view.dart';
+export 'package:festo_post/module/login_module/view/otp_view.dart';
+export 'package:festo_post/module/login_module/view/referral_view.dart';
+export 'package:festo_post/module/login_module/view/register_view.dart';
+export 'package:festo_post/module/login_module/view/user_auth_view.dart';
+export 'package:festo_post/module/info_module/view/info_view.dart';
+export 'package:festo_post/module/imageEdit_module/view/imageEdit_view.dart';
+export 'package:festo_post/module/imageEdit_module/view/edittingBottomSheet.dart';
+export 'package:festo_post/module/imageEdit_module/view/stickerBottomSheet.dart';
+export 'package:festo_post/module/frameEditing_module/view/frameEditing_view.dart';
+export 'package:festo_post/module/dashboard_module/view/dashboard_view.dart';
+export 'package:festo_post/module/dashboard_module/view/SeetingDrawer.dart';
